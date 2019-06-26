bda505cf26fc158718834241eacd05dbc962eea7
Kalit Inani <inanikalit31@gmail.com>
2019-06-27 01:48:21 +0530

    Added functionality to retrieve data from database.

--- app/src/main/java/com/example/foodapp/activity/ANCact.java
+++ app/src/main/java/com/example/foodapp/activity/ANCact.java
@@ -2,20 +2,46 @@ package com.example.foodapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodapp.R;
+import com.example.foodapp.adapters.RecycleAdapter;
+import com.example.foodapp.fragments.FragANCParatha;
+import com.example.foodapp.models.Item;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ANCact extends AppCompatActivity {
 
     private Button cart_butt;
+    private RecyclerView recyclerView;
+    private RecycleAdapter adapter;
+    ArrayList<Item> items;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myRef;
+    public static final String TAG = "Tag";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ancact);
+        FirebaseApp.initializeApp(this);
 
         cart_butt = findViewById(R.id.cart_butt);
         cart_butt.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +52,48 @@ public class ANCact extends AppCompatActivity {
             }
         });
 
-    }
+        recyclerView = findViewById(R.id.anc_rv);
 
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference("ANC").child("Parathas");
+
+        items = new ArrayList<>();
+
+
+     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                try {
+                    items.clear();
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        Item item = ds.getValue(Item.class);
+                        items.add(item);
+                    }
+
+                    RecycleAdapter adapter = new RecycleAdapter(items);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                    recyclerView.setHasFixedSize(true);
+                    recyclerView.setAdapter(adapter);
+                }
+              catch (Exception e)
+               {
+                   Toast.makeText(getApplicationContext(), e.toString(),Toast.LENGTH_LONG).show();
+               }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(),databaseError.toString(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+    }
 }
